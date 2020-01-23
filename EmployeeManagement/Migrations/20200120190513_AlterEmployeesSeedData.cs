a954ed75c9764b7052046557dd29c6bfd6c3edfe
Burak ARAT <burak_arat@hotmail.com>
2020-01-23 21:03:32 +0300

    Importing Project

---
+++ EmployeeManagement/Migrations/20200120190513_AlterEmployeesSeedData.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeManagement.Migrations
+{
+    public partial class AlterEmployeesSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Email", "Name" },
+                values: new object[] { "mahmut@gmail.com", "Mahmut" });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "Department", "Email", "Name" },
+                values: new object[] { 2, 1, "ayse@gmail.com", "Ayşe" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Email", "Name" },
+                values: new object[] { "burak2arat@gmail.com", "Burak" });
+        }
+    }
+}
---
+++ EmployeeManagement/Models/ModelBuilderExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.Models
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasData(
+                new Employee
+                {
+                    Id = 1,
+                    Name = "Mahmut",
+                    Department = Dept.IT,
+                    Email = "mahmut@gmail.com"
+                },
+                new Employee
+                {
+                    Id = 2,
+                    Name = "Ayşe",
+                    Department = Dept.HR,
+                    Email = "ayse@gmail.com"
+                }
+            );
+        }
+    }
+}
